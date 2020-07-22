f943408f2ca6e6a740f7827db27e649cc3c8444f
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-07-22 23:51:56 +0200

    Frontend initialisation (Vue + BootstrapVue)

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <b-button variant="success" v-b-tooltip="{title: 'And so is Tooltip...'}">
+            BootsrapVue is here !
+        </b-button>
+    </div>
+</template>
+
+<script>
+
+</script>
